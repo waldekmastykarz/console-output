aad71aa935997de8cbb4c2d454ed20decc5959da
waldekmastykarz <waldek@mastykarz.nl>
2022-12-17 15:17:31 +0100

    .

--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 
 Write("← ← intercepted request");
 WriteWithColor("↑ ↑ request passed thru to Graph", ConsoleColor.DarkGreen);
-WriteWithColor(" !  Warning", ConsoleColor.Yellow);
-WriteWithColor(" i  Tip", ConsoleColor.Blue);
+WriteWithColor("/!\\ Warning", ConsoleColor.Yellow);
+WriteWithColor("(i) Tip", ConsoleColor.Blue);
 WriteWithColor("x → Failed", ConsoleColor.DarkRed);
 WriteWithColor("o → Mocked", ConsoleColor.DarkYellow);
 
@@ -13,11 +13,11 @@ Console.WriteLine();
 
 Write("← ← GET https://graph.microsoft.com/v1.0/me?$select=id");
 WriteWithColor("↑ ↑ GET https://graph.microsoft.com/v1.0/me?$select=id", ConsoleColor.DarkGreen);
-WriteWithColor(" !  To improve performance of your application, use the $select parameter. More info at https://learn.microsoft.com/graph/query-parameters#select-parameter", ConsoleColor.Yellow);
-WriteWithColor(" i  To handle API errors more easily, use the Graph SDK. More info at https://aka.ms/move-to-graph-js-sdk", ConsoleColor.Blue);
-WriteWithColor("x → 429 Too Many Requests", ConsoleColor.DarkRed);
-WriteWithColor("x → Calling https://graph.microsoft.com/v1.0/me?$select=id again before waiting for the Retry-After period. Request will be throttled", ConsoleColor.Red);
-WriteWithColor("o → 200 OK https://graph.microsoft.com/v1.0/me*", ConsoleColor.DarkYellow);
+WriteWithColor("/!\\ To improve performance of your application, use the $select parameter. More info at https://learn.microsoft.com/graph/query-parameters#select-parameter", ConsoleColor.Yellow);
+WriteWithColor("(i) To handle API errors more easily, use the Graph SDK. More info at https://aka.ms/move-to-graph-js-sdk", ConsoleColor.Blue);
+WriteWithColor("× → 429 Too Many Requests", ConsoleColor.DarkRed);
+WriteWithColor("× → Calling https://graph.microsoft.com/v1.0/me?$select=id again before waiting for the Retry-After period. Request will be throttled", ConsoleColor.Red);
+WriteWithColor("○ → 200 OK https://graph.microsoft.com/v1.0/me*", ConsoleColor.DarkYellow);
 
 Console.WriteLine();
 Console.WriteLine();
